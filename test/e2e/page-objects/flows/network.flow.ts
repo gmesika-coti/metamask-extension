ae0c5ad51484342c5c19ad7ad1c212a35b1e4813
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-05-25 17:26:50 +0200

    chore: Tron E2E structure and initial tests

--- test/e2e/page-objects/flows/network.flow.ts
+++ test/e2e/page-objects/flows/network.flow.ts
@@ -2,6 +2,14 @@ import { Driver } from '../../webdriver/driver';
 import AssetListPage from '../pages/home/asset-list';
 import NetworkManager from '../pages/network-manager';
 
+export async function clearOrphanedNetworkManagerBackdrop(
+  driver: Driver,
+): Promise<void> {
+  await driver.executeScript(
+    `document.querySelectorAll('.modal__backdrop').forEach((el) => el.remove());`,
+  );
+}
+
 export const switchToNetworkFromNetworkSelect = async (
   driver: Driver,
   networkCategory: string,
@@ -10,10 +18,10 @@ export const switchToNetworkFromNetworkSelect = async (
   console.log(
     `Switching to network: ${networkName} in category: ${networkCategory}`,
   );
-  const assetListPage = new AssetListPage(driver);
   const networkManager = new NetworkManager(driver);
 
-  await assetListPage.openNetworksFilter();
+  await networkManager.openNetworkManager();
   await networkManager.selectTab(networkCategory);
   await networkManager.selectNetworkByNameWithWait(networkName);
+  await clearOrphanedNetworkManagerBackdrop(driver);
 };
