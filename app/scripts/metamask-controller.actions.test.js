64b001f26bc4cdeaf5e60c82b94f12f48a9c3a6c
Guillaume Roux <guillaumeroux123@gmail.com>
2026-05-25 12:08:24 +0200

    cleanup

--- app/scripts/metamask-controller.actions.test.js
+++ app/scripts/metamask-controller.actions.test.js
@@ -25,6 +25,7 @@ import { HardwareKeyringNames } from '../../shared/constants/hardware-wallets';
 import { FirstTimeFlowType } from '../../shared/constants/onboarding';
 import { ExtensionPasskeyErrorCode } from '../../shared/lib/passkey/passkey-error';
 import MetaMaskController from './metamask-controller';
+
 const mockToHardwareWalletError = jest.fn();
 const mockIsUserRejectedHardwareWalletError = jest.fn().mockReturnValue(false);
 
